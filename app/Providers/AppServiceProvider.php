2b886dbc79afdbf8920a9630a940d1c73d42aa2e
AKASH\matainja <matainja0106@gmail.com>
2026-01-15 15:13:20 +0530

    price list

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Storage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Share priceListUrl with all views
+        View::composer('*', function ($view) {
+            $files = Storage::disk('public')->files('pricelists');
+            if (!empty($files)) {
+                usort($files, function ($a, $b) {
+                    return Storage::disk('public')->lastModified($b) <=> Storage::disk('public')->lastModified($a);
+                });
+                $latestFile = basename(array_shift($files));
+                $priceListUrl = route('admin.pricelist.show', ['filename' => $latestFile]);
+            } else {
+                $priceListUrl = '#'; // Default link if no file is found
+            }
+            
+            $view->with('priceListUrl', $priceListUrl);
+        });
     }
 }
